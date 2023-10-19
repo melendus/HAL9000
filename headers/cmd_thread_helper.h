3053e795e8672ee49936b7763448cf0e2724231d
Sandu Mihai <mundarini0802@gmail.com>
2023-10-19 22:00:02 +0300

    HAL9000: lab2 - homework

---
+++ headers/cmd_thread_helper.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "cmd_common.h"
+
+FUNC_GenericCommand CmdListCpus;
+FUNC_GenericCommand CmdListThreads;
+FUNC_GenericCommand CmdReadyThreads;
+FUNC_GenericCommand CmdYield;
+FUNC_GenericCommand CmdRunTest;
+FUNC_GenericCommand CmdSendIpi;
+FUNC_GenericCommand CmdListCpuInterrupts;
+FUNC_GenericCommand CmdTestTimer;
+FUNC_GenericCommand CmdCpuid;
+FUNC_GenericCommand CmdRdmsr;
+FUNC_GenericCommand CmdWrmsr;
+FUNC_GenericCommand CmdCheckAd;
+FUNC_GenericCommand CmdSpawnThreads;
